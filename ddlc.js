7cfc3d09a5251ed6918931855628e4d18094c999
Esadai Studios <138761328+esadai-studios@users.noreply.github.com>
2023-07-06 00:33:35 -0700

    Add files via upload

---
+++ ddlc.js
@@ -0,0 +1,14 @@
+function showImage(imageSrc) {
+    var modal = document.getElementById("imageModal");
+    var modalImage = document.getElementById("modalImage");
+
+    modal.style.display = "block";
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
+
+//separacion epica//
